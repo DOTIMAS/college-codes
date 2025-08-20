e3cbf8080bdfbd307c583658263348d27730145e
DOTIMAS <152352332+DOTIMAS@users.noreply.github.com>
2025-08-20 17:29:17 +0800

    Add files via upload

---
+++ appdev/Website/favorite.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<div class="MyFavorite" style="text-align: center;">
+    <h1>MY FAVORITE LIST</h1>
+    <h2>FAVORITE FOOD</h2>
+    <img src="adobo.jpg" alt="Chicken Adobo" width="300" height="200"/>
+    <p>
+        I really cant get enough of this food even though we always eat eat this, it still my favorite dish, i think its engraved with my ancestry? hahaha<br>
+        but kidding aside, i really like this food every since i was young, whenever i ask my mom or grandma what we will be having for dinner <br>
+        and they said its adobo, i was so happy i really looked to it. It tastes comforting which reminds me when i was young that even til now<br>
+        i still had the sense of it.
+    </p>
+</div>
+        
+,<div class="faveartist" style="text-align: center;">
+    <h2>FAVORITE ARTIST</h2>
+    <img src="daniel.jpg" alt="DanielPadilla" width="300" height="200">
+
+
+    <h3 class="daniel">Daniel Padilla</h3>
+
+
+    <p>I dont have a specific favorite artist but whenever i think about someone whose<br> an artist the first person that comes to my mind is Daniel Padilla.<br>
+        He's one of the most controversial yet successful person rightnow because of his looks<br> and recent issues yet i still find his acting skills appaling
+        so i thinks its<br> one of the reasons why i chose him to for these specific task</p>
+
+</div>
+
+
+<div style="text-align: center;">
+    <h2>FAVORITE MUSIC</h2>
+    <iframe 
+        width="640" 
+        height="360" 
+        src="https://youtu.be/OxJ2U5xcb6s?si=wHyT4wHHETlj0bKN" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+    </iframe>
+</div>
+
+<body class="design" style="background-color: aqua;">
+    
+</body>
+</html>
+
+<div style="text-align: center;">
+    <h2>FAVORITE MOVIE</h2>
+    <iframe 
+        width="640" 
+        height="360" 
+        src="https://youtu.be/N2fdsLVuFkY?si=X3Ow8F7KlrlG9rXM" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+    </iframe>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ appdev/Website/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>"LOOKING AT THE INTERNET"</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="favorite.html" href="style.css">  
+</head>
+
+<header>
+    <h1>"MY LIFE IN A NUTSHELL"</h1>
+</header>
+
+<div class="personalife">
+    <h2>My Personal Life</h2>
+    <p>
+        I dont Feel too comfortable telling other people my personal so i make it most of the time a vague story<br> I have 4 siblings and i am the oldest one.<br> 
+        our father left us when i think i was 9 years old but he continue to support us for a few months until he totally left us<br> the burden of having 4 children has been placed on my mother
+        <br> and thats how we became unfortunate for a few more years until my mom decided that she wants to become OFW<br> she left us and placed us in a care of our grandmother and after that we turned out good
+        <br>As of today my mom has still outside our Country and all of us are still studying<br> we all know the sacrifices of our mom and grandma so thats why we continue to fight in order for us to retire our mother
+        <br> And that is why i am doing my best to continously learn and have a good grades.<br> Since i am the breadwinner of my family after my mom i ensure that i always did my best.
+    </p>
+</div>
+
+
+<div class="SchoolLife">
+    <h2>My School Life</h2>
+    <h3>Elementary Years</h3>
+    <p>
+        Its been a long time but my memories of my elementary years are still fresh in my mind. Since i was young it's my very nature that i am<br>
+        playful, even though i cant buy anything i still dont see it as a bad way. I am struggling with tools and equipments yet i am still not<br>
+        bothered by it that much. The only that comes to my mind in my elementary years is how i can play despite of classes. I really thought <br>
+        it is funny because i see the studies and lessons for every subject as a hindrance for me to play with. Yet i still do good in classes,<br>
+        it is engraved in my soul that i must always do my very best for me to make my family proud. In these years i cn still balance the time<br>
+        of playing and studying but what really makes me remember these years is the school, the friends that i built, the classes that takes too<br>
+        long to finish, the times where i went overboard with my playfulness that leads to me being sanctioned. I can still vaguely remember it <br>
+        like it was happened yesterday.
+    </p>
+</div>
+
+
+<div class="HSLife">
+    <h3>High School Years</h3>
+    <p>
+        In terms of having too many memories my high school has completely filled it up. I can still remember the places, the sections that ive<br>
+        been with, the people that i make friends with, the terror teachers, and the strict guard. These school years has become one of the main<br>
+        reasons of who i am today, it creates me, shelters me and helped me understand what i am and what i want to become. Whenever i think about<br>
+        my high school years i cant help but laugh and my heart aches at the same time. I was really happy because ive had too many friends that <br>
+        i bonded with yet it kinda hurts knowing that those friends will never comeback since my path will not corrolate with them yet i am still<br>
+        happy for all of us because i know that no matter what path weve chosen i know that what we really shared when we are still in high school<br>
+        will be remembered. So instead of being too sad about it i take it as part of fate that no matter what we do it just happens and we cant <br>
+        control it so we just need to accept it.
+    </p>
+</div>
+
+
+<div class="CollegeLife">
+    <h3>College Years so far</h3>
+    <p>
+        So far my college years has become so-so, i still remember the time that i enrolled at St. Bernadette institution, i was scared and happy <br>
+        at the same time knowing full to well that this path will pave my path for a successful life. Luckily, i easily create a friends since i <br>
+        have classmate that i am affiliated with in my high school years even though were not that close but we eventually bonded with since we need<br>
+        to help out each other. After that every falls into pieces, i studied hard even though mostly the classes are online and it makes me sleepy<br>
+        but i knew that i have to give my best. After few weeks we started to have a face to face meetings and that is when i meet my new classmtes,<br>
+        professors that is totally a new environment from me. I start to struggle from going to school because i stil havent adjust since my school <br>
+        in elementary and high school are both near my house but i still prefer to do face to face since i can learn more. So far my college life <br>
+        has been pretty tight i have many unsubmitted activities that i need to pass addition to my own personal responsibilities as a son, grandson,<br>
+        brother and being a part of catholic organization but i still believe that everything will be alright as long as i do my best and to always<br>
+        remember what i am today and focusing more on my life and continously improving my skills.
+    </p> 
+</div>
+          
+<body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="favorite.html">MY FAVORITE LIST</a>
+                </li>
+            </ul>
+        </nav>
+ 
+</body>
+</html>
